599461e33053f28eba37cc73d6a67fc767904e2c
Abdulrahman Elheyb <45883300+Abdulrahmanelheyb@users.noreply.github.com>
2021-11-28 14:01:32 +0300

    Create Validation.cs
    Added validation class

---
+++ APICore/Validation.cs
@@ -0,0 +1,16 @@
+﻿// Created By abdulrahman elheyb
+// 2021-11-28 1:57 PM
+
+using System.Text.RegularExpressions;
+
+namespace APICore
+{
+    public static class Validation
+    {
+        public static bool Email(string text)
+        {
+            var rgx = new Regex(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$");
+            return rgx.IsMatch(text);
+        }
+    }
+}

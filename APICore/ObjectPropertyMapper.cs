acd192ad32bf7ebab169b946898982ecbce60fd9
Abdulrahman Elheyb <45883300+Abdulrahmanelheyb@users.noreply.github.com>
2021-11-30 09:57:30 +0300

    Add ObjectPropertyMapper.cs
    Added ObjectPropertyMapper class, GetObjectProperties function returns only properties names that have values

---
+++ APICore/ObjectPropertyMapper.cs
@@ -0,0 +1,68 @@
+﻿// Created By abdulrahman elheyb
+// 2021-11-28 9:34 PM
+
+using System;
+using System.Collections.Generic;
+
+namespace APICore
+{
+
+    public enum TestTypes
+    {
+        One = 0,
+        Two = 1,
+        Three = 2
+    }
+
+    /// <summary>
+    /// Represents 
+    /// </summary>
+    public static class ObjectPropertyMapper
+    {
+        /// <summary>
+        /// This gets only object properties names when properties have values 
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static IEnumerable<string> GetObjectProperties(object obj)
+        {
+            var props = obj.GetType().GetProperties();
+            var propsNames = new List<string>();
+            foreach (var prop in props)
+            {
+                var propertyValue = prop.GetValue(obj);
+                
+                if(propertyValue is null)
+                    continue;
+
+                if (prop.PropertyType == typeof(string))
+                    if (string.IsNullOrEmpty(propertyValue.ToString()))
+                        continue; 
+                
+                if (prop.PropertyType == typeof(int))
+                    if ((int)propertyValue < 1)
+                        continue;
+                
+                if (prop.PropertyType == typeof(double))
+                    if ((double)propertyValue is 0d)
+                        continue;
+                
+                if (prop.PropertyType == typeof(float))
+                    if ((float)propertyValue is 0f)
+                        continue;
+                
+                if (prop.PropertyType.IsEnum)
+                    if ((TestTypes)propertyValue == 0 )
+                        continue;
+
+                if (prop.PropertyType == typeof(DateTime))
+                    if((DateTime)propertyValue == DateTime.MinValue)
+                        continue;                
+                
+                propsNames.Add(prop.Name);
+            }
+
+            return propsNames;
+        }
+    }
+}
--- Laboratory/Program.cs
+++ Laboratory/Program.cs
@@ -7,12 +7,22 @@ namespace Laboratory
     {
         private static void Main()
         {
-            SqlQueryParamizer.BaseFolder = "Queries";
-            var x = new SqlQueryParamizer("users", "getlist");
-            var param = x.GetParameter("My Name");
-            param.SetValue(DateTime.Now.ToString("yyyy-MM-dd"));
-            var z = x.Query;            
-            Console.WriteLine(z);
+            // SqlQueryParamizer.BaseFolder = "Queries";
+            // var x = new SqlQueryParamizer("users", "getlist");
+            // var param = x.GetParameter("My Name");
+            // param.SetValue(DateTime.Now.ToString("yyyy-MM-dd"));
+            // var z = x.Query;   
+
+            var x = new
+            {
+                Name = "Abdulrahman",
+                Age = 19,
+                IsStudent = true,
+                Degree = 89.0f
+            };
+            
+            var z = ObjectPropertyMapper.GetObjectProperties(x);
+            Console.WriteLine();
             Console.ReadKey();
         }
     }

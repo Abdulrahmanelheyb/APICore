66ebeec757224701f2d23efac172e777d2a16f57
Abdulrahman Elheyb <45883300+Abdulrahmanelheyb@users.noreply.github.com>
2021-12-01 17:03:34 +0300

    Update
    Added patch message and changed Response API message property type to string array

--- APICore/Response.cs
+++ APICore/Response.cs
@@ -10,8 +10,7 @@ namespace APICore
     public struct Response<T>
     {
         public bool Status { get; set; }
-        public string Message { get; set; }
-        public List<string> Messages { get; set; } 
+        public string[] Messages { get; set; } 
         public T Data { get; set; }
     }
 }
